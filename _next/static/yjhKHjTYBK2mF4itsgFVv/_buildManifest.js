99a739a05dc463ef857b510c1229a45f9124d097
vicarb <108682674+vicarb@users.noreply.github.com>
2022-09-01 16:13:34 +0000

    Deploying to gh-pages from @ vicarb/vicarb.github.io@a62dd8b217284a62c34801095c06fe83795761bc 🚀

--- _next/static/yjhKHjTYBK2mF4itsgFVv/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/844-07046a867c1de7ef.js","static/css/991e47cf81f3e010.css","static/chunks/pages/index-b9150d3f2a74f67a.js"],"/_error":["static/chunks/pages/_error-29b3d7bb596dbd7b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
