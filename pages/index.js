773c72645df6f7152362b685b77c052631eda265
vicarb <108682674+vicarb@users.noreply.github.com>
2022-09-07 21:06:57 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -59,6 +59,9 @@ export default function Home() {
         </div>
       </motion.main>
 
+      <div className='m-12'>
+        <Swipe/>
+      </div>
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -69,9 +72,7 @@ export default function Home() {
          
         </a>
       </footer>
-        <div className='m-12'>
-        <Swipe/>
-        </div>
+
     </div>
   )
 }
